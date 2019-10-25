cddd94fc5dfd667153018f457705d28d67459eb2
Vashira <vashimgs@gmail.com>
2019-10-25 18:58:05 +0100

    added bubble sort method and some test

--- bubble.rb
+++ bubble.rb
@@ -1,2 +1,25 @@
-my_name = "Mario"
-puts my_name
+# frozen_string_literal: true
+
+def bubble_sort(arr)
+  loop do
+    swap = false
+    (arr.length - 1).times do |i|
+      temp = arr[i]
+      next unless arr[i] > arr[i + 1]
+
+      swap = true
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+    end
+    break unless swap
+  end
+  arr
+end
+
+array = []
+(1..10_000).each do |x|
+  array.push(x)
+end
+array.reverse!
+
+puts bubble_sort(array)
