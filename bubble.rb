33655c71c58a227f2fbd673a68c1d228603d756d
Mario Barrios <mariobarriossalazar@outlook.com>
2019-10-25 14:56:18 -0600

    Added bubble_sort_by method

--- bubble.rb
+++ bubble.rb
@@ -1,25 +1,42 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
+  iterations = 0
   loop do
     swap = false
-    (arr.length - 1).times do |i|
-      temp = arr[i]
+    (arr.length - 1 - iterations).times do |i|
       next unless arr[i] > arr[i + 1]
 
+      temp = arr[i]
       swap = true
       arr[i] = arr[i + 1]
       arr[i + 1] = temp
     end
     break unless swap
+
+    iterations += 1
   end
   arr
 end
 
-array = []
-(1..10_000).each do |x|
-  array.push(x)
-end
-array.reverse!
+def bubble_sort_by(arr)
+  iterations = 0
+  loop do
+    swap = false
+    (arr.length - 1 - iterations).times do |i|
+      value = yield(arr[i], arr[i + 1])
+      next unless value.positive?
 
-puts bubble_sort(array)
+      temp = arr[i]
+      swap = true
+      arr[i] = arr[i + 1]
+      arr[i + 1] = temp
+    end
+    break unless swap
+
+    iterations += 1
+  end
+  arr
+end
+
+
